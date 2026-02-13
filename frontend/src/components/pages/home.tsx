72702ebb8d3b7b728869293f76d3f9a1aed26bb6
daffarahman <69158099+daffarahman@users.noreply.github.com>
2026-02-13 11:48:00 +0700

    add: axios, router, shadcn

---
+++ frontend/src/components/pages/home.tsx
@@ -0,0 +1,11 @@
+import { Button } from "../ui/button";
+
+export default function HomePage() {
+    return (
+        <>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+        </>
+    )
+}
