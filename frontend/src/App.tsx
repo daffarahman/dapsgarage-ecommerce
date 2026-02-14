cc5a6fcb2d5f847a41f03eec1ed918c8c7244e55
daffarahman <69158099+daffarahman@users.noreply.github.com>
2026-02-14 12:31:48 +0700

    add: lenis for smooth scrolling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,18 +3,21 @@ import HomePage from "./components/pages/home";
 import ProductsPage from "./components/pages/products";
 import Navbar from "./components/navbar";
 import { ThemeProvider } from "./components/theme-provider";
+import { LenisProvider } from "./components/lenis-provider";
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="system" storageKey="dapsgarage-ui-theme">
-      <Navbar />
-      <main className="min-h-screen container mx-auto p-4">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/shop-all" element={<ProductsPage />} />
-        </Routes>
-      </main>
-    </ThemeProvider>
+    <LenisProvider>
+      <ThemeProvider defaultTheme="system" storageKey="dapsgarage-ui-theme">
+        <Navbar />
+        <main className="min-h-screen container mx-auto p-4">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/shop-all" element={<ProductsPage />} />
+          </Routes>
+        </main>
+      </ThemeProvider>
+    </LenisProvider>
   );
 }
 
